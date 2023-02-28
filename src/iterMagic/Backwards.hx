9a187b6860f0bff89d26b485971799624b3945d6
nanjizal <nanjizal@users.noreply.github.com>
2023-02-28 11:30:40 +0000

    Create Backwards.hx

---
+++ src/iterMagic/Backwards.hx
@@ -0,0 +1,34 @@
+package iterMagic;
+import iterMagic.IntIterStart;
+/*
+class Test {
+  static function main() {
+    for( i in (( 0...10 ): Backwards ) ){
+			trace( i );
+    }
+  }
+}
+*/
+@:transitive
+@:access( IntIterator.min, IntIterator.max )
+abstract Backwards( IntIterStart ) from IntIterStart {
+    public inline
+    function new( min: Int, max: Int ){
+        this = new IntIterStart( min, max );
+    }
+    @:from
+    static inline
+    public function fromIterator( ii: IntIterator ): Backwards {
+        return new Backwards( ii.min, ii.max );
+    }
+    @:to
+    function toIterStart():Backwards {
+       return new Backwards( this.start, this.max );
+    }
+	public inline function hasNext() {
+		return this.max > this.start;
+	}
+	public inline function next() {
+		return ( this.max-- -1 );
+	}
+}
