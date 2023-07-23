b68b3ff54ec0a602d666d48c1acce920fe0930b4
nanjizal <nanjizal@users.noreply.github.com>
2023-07-23 12:02:50 +0100

    Update Tmodfiy.hx add Tmodify2

--- src/iterMagic/Tmodfiy.hx
+++ src/iterMagic/Tmodfiy.hx
@@ -10,3 +10,44 @@ abstract Tmodify<T>( T ) to T from T {
     }
   }
 }
+class Tstruct<T> {
+  public var t:T;
+  public var update: Tupdate<T>;
+  public function new( t: T ){
+     this.t = t;
+  }
+}
+// too messy but interesting..
+@:forward
+abstract Tmodify2<T>(Tstruct<T>){
+  public inline function new(t: T ){
+    this = new Tstruct<T>( t );
+    this.update = createUpdate();
+  }
+  public inline function createUpdate(): Tupdate<T> {
+	return function(t:T):T {
+		this.t = t;
+		return t;
+	}
+  }
+  @:to
+  public function toTmodify(): T{
+    var s = this;
+    return s.t;
+  }
+  @:from
+  public static inline function fromTmodify<T>( t: T ): Tmodify2<T> {
+	return new Tmodify2<T>(t);
+  } 
+}
+/*
+function main() {
+	var i:Tmodify2<Int> = 0;
+	plus1( i );
+	trace( (i:Int ) );
+}
+
+function plus1(i:Tmodify2<Int> ) {
+	i.update( (i:Int) + 1 );
+}
+*/
