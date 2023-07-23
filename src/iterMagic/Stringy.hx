7f686c0aa0f22f9fef24033d137924ba7ebda807
nanjizal <nanjizal@users.noreply.github.com>
2023-07-23 01:27:24 +0100

    Update Stringy.hx

--- src/iterMagic/Stringy.hx
+++ src/iterMagic/Stringy.hx
@@ -4,7 +4,7 @@ abstract Stringy( String ) to String from String {
   public inline function new( s ) this = s;
   public var updater( get, never ): StringUpdate;
   inline function get_updater(): StringUpdate {
-	  final sReturn = function( s: String ): String {
+	final sReturn = function( s: String ): String {
       this = s;
       return s;
     }
