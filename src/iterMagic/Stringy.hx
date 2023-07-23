153503fdced556675e14bb1e30a4e45da1120e09
nanjizal <nanjizal@users.noreply.github.com>
2023-07-23 01:25:35 +0100

    Create Stringy.hx

---
+++ src/iterMagic/Stringy.hx
@@ -0,0 +1,13 @@
+package iterMagic;
+typedef StringUpdate = ( String )->String;
+abstract Stringy( String ) to String from String {
+  public inline function new( s ) this = s;
+  public var updater( get, never ): StringUpdate;
+  inline function get_updater(): StringUpdate {
+	  final sReturn = function( s: String ): String {
+      this = s;
+      return s;
+    }
+    return sReturn;
+  }
+}
