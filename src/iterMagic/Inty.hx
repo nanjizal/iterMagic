93aa9cf54e57ee93a2b62f802bb3844fdf3e3d03
nanjizal <nanjizal@users.noreply.github.com>
2023-07-23 01:28:44 +0100

    Create Inty.hx

---
+++ src/iterMagic/Inty.hx
@@ -0,0 +1,13 @@
+package iterMagic;
+typedef IntUpdate = ( Int )->Int;
+abstract Inty( Int ) to Int from Int {
+  public inline function new( s ) this = s;
+  public var updater( get, never ): IntUpdate;
+  inline function get_updater(): IntUpdate {
+	  final iReturn = function( i: Int ): Int {
+      this = i;
+      return i;
+    }
+    return iReturn;
+  }
+}
