d76d47a3f924689d35e0434e5025a35b52ae73d6
nanjizal <nanjizal@users.noreply.github.com>
2023-07-23 01:26:36 +0100

    Update Floaty.hx

--- src/iterMagic/Floaty.hx
+++ src/iterMagic/Floaty.hx
@@ -3,8 +3,8 @@ typedef FloatUpdate = ( Float )->Float;
 abstract Floaty( Float ) to Float from Float {
   public inline function new( f ) this = f;
   public var updater( get, never ): FloatUpdate;
-  inline function get_updater(): FloatUpdate {
-	  final fReturn = function( f: Float ): Float {
+  inline function get_updater(): FloatUpdate { 
+    final fReturn = function( f: Float ): Float {
       this = f;
       return f;
     }
